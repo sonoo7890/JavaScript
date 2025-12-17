79d804532e46b9e4254de8ec831793613b719c31
Adarsh Gour <adarshgour082@gamil.com>
2025-12-17 13:01:54 +0530

    added BOM.js file

---
+++ BOM02.js
@@ -0,0 +1,22 @@
+var a=5
+console.log(a);
+function sonu(){
+
+    console.log("hello i am sonu");}
+function moni(){
+   var a=5
+   var b='5'
+   //a++=>a=a+1
+   //--a => a=a-1
+   console.log(a+b);
+   console.log(++a);
+   console.log(a==b); //camparison operator
+   console.log(a===b);// camparision ope. datatype check karta hai (false)
+
+   var c=5
+   var d='6'
+   console.log(c+d);  //'56'
+}
+moni()
+//sonu()        //function ko call karna show ya print karne ke liye
+
