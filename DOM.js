334e5470aa5cc19746c547f55a4c563d1d1ec0c0
Adarsh Gour <adarshgour082@gamil.com>
2025-12-20 12:44:41 +0530

    added DOM.js file

---
+++ DOM.js
@@ -0,0 +1,49 @@
+let sonu=()=>{
+
+document.getElementsByClassName()
+document.getElementById("")
+ document.querySelector('')     //class=> .  id=>#
+document.querySelectorAll('')
+
+let Ankit=document.querySelector('#yes')
+let name="Arvind"
+Ankit.innerText="This text is <i> from </i> JS"
+Ankit.innerText = `This text is for ${name}<i>form</i> js`
+Ankit.style.color="red"
+Ankit.style.backgroundColor="green"
+
+}
+
+
+let hidee=()=>{
+let para=document.querySelector('#str')
+para.style.display="none"
+}
+
+let showw=()=>{
+let text=document.querySelector('#str')
+text.style.display="block"
+}
+
+
+let togg=()=>{
+    let okk= document.querySelector('#te')
+    if(okk.style.display=="block"){
+        okk.style.display="none"
+
+    }
+    else{
+        okk.style.display="block"
+        okk.style.backgroundImage="url()"
+    }
+
+
+}
+
+
+let show=()=>{
+    
+}
+
+
+
