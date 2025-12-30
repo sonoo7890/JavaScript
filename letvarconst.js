330264e24ebde82109361a88dc43802942725692
Adarsh Gour <adarshgour082@gamil.com>
2025-12-30 12:44:01 +0530

    added letverconst.js file

---
+++ letvarconst.js
@@ -0,0 +1,44 @@
+// vaar , let, const me differance..........................
+//let=> redeclear
+function zainab(){
+
+//=====================Redeclare========
+// var a
+// var a
+
+// let b
+// let b
+
+// const c
+// const c
+
+
+//===================Reassignment====================
+// var a=25
+// a=26
+// console.log(a)
+
+// let b=25
+// b=26
+// console.log(b)
+
+// const c=25
+// c=26
+// console.log(c)
+
+
+ //==============================Scope=================
+ var a=5  //var fuction scope hai
+ {
+
+ }
+ //let = block scope
+//const=block scope
+
+
+//====================Hoisting===========================
+
+console.log(b)
+var b=5
+}
+zainab()
