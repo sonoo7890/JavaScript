664360900029cf2d407d049d4c56ba6ccb1569be
Adarsh Gour <adarshgour082@gamil.com>
2026-01-09 13:02:06 +0530

    added array.js file

---
+++ Array.js
@@ -0,0 +1,38 @@
+// alert("this is from 'cybrom'of 'mp nager'only")
+
+//template letrals ( `` )
+
+// 1st use it supports single and double quotes
+  let str=`This is 'string' from "cybrom"`
+  // alert(str)
+
+  //2nd use it supports multi line string
+   let str2=`hdfhgjhb jghkjlj jkhlkhjgh hjkl
+            jhkjkljkhk jhkjjyyute4werd hhuiuopifdser`
+
+  //3rd use it supports html tags
+   let str3='this is <i> cybrom </i> of mp nagar'
+   document.write(str3)
+
+   //4th use it supports dynamic value or variable
+   let myname="sonu"
+   let myage=19
+
+   //alert('i am'+myname+ 'and i am'+myage +'year old')
+   alert(`i am ${myname} and i am ${myage} year old`)
+
+
+   //Array=>collection of diffreance datatype elements
+
+   let arr=["Apple","Mango","Banana","Grapes"]
+   let arr2=["Dragon","avacado"]
+   //alert(arr[3]).... Grapes   access element
+
+   // alert(arr.length)
+   //alert(arr.concat(arr2))
+   //cosole.log (arr.slice(1,3)).....ak se jyada method nikalne ke liye slice ka use kiya jata hai or ak ke liye[]
+
+   // Array 4 method=> pop(),push(),shift(),unshift() 
+   //pop=> array last element remove karta hai
+   //push=> array last element add karta hai
+   //shift=>
