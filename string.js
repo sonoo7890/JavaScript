fc1e51947b8b1c22aa5f80dacb3e9ecbb907e973
Adarsh Gour <adarshgour082@gamil.com>
2026-01-17 12:52:50 +0530

    added string file

---
+++ string.js
@@ -0,0 +1,46 @@
+//var myname = 'This is string'
+ //var myage = '18'
+ //var city="Bhopal"
+
+ //METHOD
+ //console.log(myname.length)
+///myname[0]//......  indexing number 'T'
+ //console.log(myname.concat(myage));  //concat-> do ya jyada string ko add karna
+ //console.log(myname.concat(myage,city));
+
+
+// function deepu(){
+//     var ans=prompt("enter number1")
+//      var ans=prompt("enter number2")
+
+
+    //string=>number=> number(), ParseInt(), ParseFloat()
+//      alert(parseInt(num1)+parseInt(num2))
+
+// }
+// deepu()
+
+//slice method
+// var abc='hello cybrom i am here' 
+// alert(abc.slice(2,8))        //l-y stop point se ak kam 
+// alert(abc.slice(0,-1))
+
+
+var fruits=["apple","bnana",123,true] // array => collection of hetrogenious datatype collection
+    // multiple value datatype
+
+
+
+//split method
+var f='Apple Bnana Mango Grapes' 
+console.log(f.split(' '));  //  "Apple","Bnana","Mango","Grapes" 
+
+var f='Apple-Bnana Man-go Grapes' 
+console.log(f.split('-'));  
+
+// tostring
+var a=6
+a.toString() //'6'
+
+//string se Array me convert
+//num se string me convert= string
